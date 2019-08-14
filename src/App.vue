738df7b0514296342f944cae3683caa0b68d5550
Wenquan Zeng <724708892@qq.com>
2019-08-14 17:54:57 +0800

    add api file

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,19 @@
 </template>
 
 <style lang="scss">
+html,
+body,
+#app {
+  height: 100%;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
---
+++ src/views/user/login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="login"></div>
+</template>
+<script>
+export default {
+  name: 'login',
+};
+</script>
+<style lang="scss"></style>
