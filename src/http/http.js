e9eb099c7f4dda15a8d0e89eccda9df8a1852a78
Wenquan Zeng <724708892@qq.com>
2019-08-14 16:31:45 +0800

    axios config

--- src/http/http.js
+++ src/http/http.js
@@ -2,8 +2,22 @@ import axios from 'axios';
 
 const http = axios.create();
 
-http.interceptors.request.use(config => {}, error => {});
+http.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    console.error(error);
+  },
+);
 
-http.interceptors.response.use(reponse => {}, error => {});
+http.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    console.error(error);
+  },
+);
 
 export default http;
