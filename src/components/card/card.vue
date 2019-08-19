6d613b2ebfb001e2c77871121a85dde197f69220
Wenquan Zeng <724708892@qq.com>
2019-08-19 10:42:06 +0800

    card

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,23 +1,91 @@
 <template>
-  <div class="custom-card">
+  <div class="custom-card" :style="{ width }">
     <div class="data">
-      <p>{{ data.name || 'Title' }}</p>
+      <p class="title">{{ data.name || 'Title' }}</p>
+      <p class="content">
+        <svg-icon class="num" iconClass="icon" />
+        <span class="value">{{ data.num }}</span>
+      </p>
     </div>
-    <div class="icon"></div>
+    <div class="icon">
+      <svg-icon
+        :style="{ fill: colorMap[data.name] }"
+        :iconClass="svgClassMap[data.name]"
+      />
+    </div>
   </div>
 </template>
 <script>
+/**
+ *总任务: #0e67f2
+ *已开展任务数: #fcb852
+ *已完成进度 #00ca89
+ * 即将到期任务数 #ff5757
+ */
 export default {
   props: {
     data: {
       type: Object,
       default: () => ({}),
     },
+    width: {
+      type: String,
+      default: '250px',
+    },
+  },
+  data() {
+    return {
+      svgClassMap: {
+        总任务数: '总任务数',
+        已开展任务数: '已开展任务',
+        总完成进度: '完成',
+        即将到期任务数: '到期',
+      },
+      colorMap: {
+        总任务数: '#0e67f2',
+        已开展任务数: '#fcb852',
+        总完成进度: '#00ca89',
+        即将到期任务数: '#ff5757',
+      },
+    };
   },
 };
 </script>
 <style lang="scss">
 .custom-card {
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 20px;
+  box-shadow: 0 0 1px $shadow-color;
+  background-color: $base-color;
+  border-radius: 3px;
+  .data {
+    p {
+      margin: 0;
+    }
+    .content {
+      padding: 10px 0 0 0;
+      display: flex;
+      align-items: center;
+    }
+    .num {
+      fill: #99c0f2;
+    }
+    .value {
+      font-weight: 700;
+      font-size: $font-xx;
+      padding-left: 10px;
+    }
+  }
+  .icon {
+    padding: 15px;
+    background-color: #f3f3f3;
+    border-radius: 100%;
+  }
+  .svg-icon {
+    height: 2em;
+    width: 2em;
+  }
 }
 </style>
--- src/views/sj/list.vue
+++ src/views/sj/list.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="progress-list"></div>
+  <div class="progress-list">
+    <div class="cards">
+      <customer-card v-for="(item, index) in data" :key="index" :data="item" />
+    </div>
+  </div>
 </template>
 <script>
+import customerCard from 'components/card/card';
 export default {
   name: 'sj-list',
+  components: {
+    customerCard,
+  },
+  data() {
+    return {
+      data: [
+        { name: '总任务数', num: 10 },
+        { name: '已开展任务数', num: 5 },
+        { name: '总完成进度', num: '30%' },
+        { name: '即将到期任务数', num: 2 },
+      ],
+    };
+  },
 };
 </script>
 <style lang="scss">
 .progress-list {
   padding: 30px 40px;
+  .cards {
+    display: flex;
+    justify-content: space-around;
+  }
 }
 </style>
