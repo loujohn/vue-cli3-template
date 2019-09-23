6b0cf20ec2a9eaa841a15c5f24536bd522654670
fish <itfocus@yeah.net>
2019-09-23 16:40:56 +0800

    配置

--- public/config.js
+++ public/config.js
@@ -1,6 +1,10 @@
 var common = 'http://192.168.11.160:8083';
 // var common = 'http://172.16.4.195:8083';
 // var common = 'http://172.16.3.239:8083';//何鹏
+
+if (!parseInt(window.location.host)) {
+  common = 'http://zrzy.cqdlzx.com';
+}
 window.config = {
   title: '矿山资源调查系统',
   baseMapUrl:
@@ -8,6 +12,3 @@ window.config = {
   baseUrl: common + '/dc-core/',
   staticUrl: common + '/dc-core/static',
 };
-if (!parseInt(window.location.host)) {
-  window.config.ksxf = 'http://zrzy.cqdlzx.com/dc-core';
-}
